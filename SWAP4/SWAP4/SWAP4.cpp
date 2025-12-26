ef1d8fcb5fd9f077ea968ca431db89cf94a3c8cd
dfghaoi <85017723+dfghaoi@users.noreply.github.com>
2025-12-27 00:03:08 +0100

    Add files via upload

---
+++ SWAP4/SWAP4/SWAP4.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <windows.h>
+using namespace std;
+int main()
+{
+    int a, b, c, d, temp;
+    cout << "A= "; cin >> a;
+    cout << "B= "; cin >> b;
+    cout << "C= "; cin >> c;
+    cout << "D= "; cin >> d;
+    cout << "A= " << a << ", B= " << b << ", C= " << c << ", D= " << d << endl;
+    temp = a;
+    a = d;
+    d = c;
+    c = b;
+    b = temp;
+    cout << "A= " << a << ", B= " << b << ", C= " << c << ", D= " << d;
+}
